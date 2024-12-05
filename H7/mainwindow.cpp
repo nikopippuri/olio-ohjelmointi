af1e3ab9e078d7d4e2e01205c22d86ced00970a2
Niko P <niko.pippuri@gmail.com>
2024-12-05 18:46:52 +0200

    Kotitehtava 7

---
+++ H7/mainwindow.cpp
@@ -0,0 +1,36 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnCount_clicked()
+{
+    counter++;
+    QString s = QString::number(counter);
+    ui->labelInfo->setText("Painiketta Count painettu " + s + " kertaa.");
+    ui->textResult->setText(s);
+}
+
+
+void MainWindow::on_btnReset_clicked()
+{
+    counter = 0;
+    QString s = QString::number(counter);
+
+    ui->labelInfo->setText("Painiketta Count painettu " + s + " kertaa");
+    ui->textResult->setText("0");
+}
+
